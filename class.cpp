2a813aec4afa4a8e800152d6150efab80c1bc56d
JOD-69 <flamehashira30@gmail.com>
2022-12-03 22:50:33 +0530

    New Code

--- class.cpp
+++ class.cpp
@@ -1,39 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define fori(i, n) for (ll i = 0; i < n; i++)
-#define all(x) x.begin(),x.end()
-
-string nline = "\n";
-int mod = 1000000007;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-typedef long long ll;
-int a = 4;
-void solution(){
-
-    ll n;
-    cin>>n;
-    
-}
-int a = 3;
-
-
 int main(){
-
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
-int t=1;
-cin>>t;
-while(t--){
-    solution();
-}
-
-return 0;
-
+    cout<<"HEllo world"<<endl;
 }
